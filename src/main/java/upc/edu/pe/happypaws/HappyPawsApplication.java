bb5c10bfa5c68646e2690a709e1ddb717b3ac7e4
SebasR1C0 <102307907+SebasR1C0@users.noreply.github.com>
2024-09-15 20:48:07 -0500

    New Repository

---
+++ src/main/java/upc/edu/pe/happypaws/HappyPawsApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.happypaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyPawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyPawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upc/edu/pe/happypaws/HappyPawsApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.happypaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
